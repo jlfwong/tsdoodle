a5d2a445d0ebfcb3dbd6c0df5c7974319fcb3257
Jamie Wong <jamie.lf.wong@gmail.com>
2018-03-17 22:17:13 -0700

    luminous flux

--- index.ts
+++ index.ts
@@ -101,6 +101,58 @@ async function ybarPlot() {
     .attr("transform", "translate(50, 0)").call(yAxis);
 }
 
+async function bulbLuminousFlux() {
+  const h = 6.626e-34 // J s
+  const k = 1.38e-23 // JK^-1
+  const c = 3e8 // m/s
+
+  const temperature = 3000 // K
+  const solidAngleOfSphere = 4 * Math.PI // sr
+  const surfaceArea = (100/60) * 0.81 * 4e-6 // m^2
+
+  function B(wav: number, T: number) {
+    return (2 * h * Math.pow(c, 2)) / Math.pow(wav, 5) * (1 / (Math.exp(h * c / (wav * k * T)) - 1))
+  }
+
+  function bulb(wav: number) {
+    return solidAngleOfSphere * surfaceArea * 0.8 * B(wav, temperature)
+  }
+
+  const rawCsv = await d3.text(require('./vl1924e.csv'))
+  let data = d3.csvParseRows(rawCsv).map(r => [parseFloat(r[0]) * 1e-9, parseFloat(r[1])])
+
+  data = data.map(d => [d[0], d[1] * bulb(d[0])])
+
+  const line = d3.line<[number, number]>()
+
+  const min = 380e-9
+  const max = 750e-9
+  const xScale = d3.scaleLinear().domain([min, max]).range([50, 650])
+  const yScale = d3.scaleLinear().domain([0, 6e7]).range([350, 50])
+
+  line
+    .x(d => xScale(d[0]))
+    .y(d => yScale(d[1]))
+    .curve(d3.curveCardinal)
+
+  const path = line(data as [number, number][])
+
+  svg.append("path")
+    .attr("d", path)
+    .attr("stroke", "#ff0000")
+    .attr("stroke-width", "1.0")
+    .attr("fill", "none")
+
+  const xAxis = d3.axisBottom(xScale).tickFormat((x: number) => (x / 1e-9).toFixed()).ticks(10)
+  const yAxis = d3.axisLeft(yScale).tickFormat((x: number) => (x / 1e9).toFixed(2)).ticks(5)
+
+  svg.append("g")
+    .attr("transform", "translate(0, 350)").call(xAxis);
+  svg.append("g")
+    .attr("transform", "translate(50, 0)").call(yAxis);
+}
+
+
 function linear2sRGB(c: number): number {
   if (c < 0.0031308) return 12.92 * c
   return 1.055 * Math.pow(c, 1/2.4) - 0.055
@@ -156,4 +208,4 @@ async function rainbow() {
 
 svg.attr('width', 700).attr('height', 400)
 
-rainbow()
+bulbLuminousFlux()
